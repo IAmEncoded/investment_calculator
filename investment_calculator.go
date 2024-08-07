8b1d5fb6b7ceb2cb5833b31dfe109499c4d15197
Conor Fearon <Dev.IAmEncoded@gmail.com>
2024-08-07 23:55:14 +0100

    Investment calculator
    Created simple console application to calculate a return on investment

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	const inflationRate = 2.5
+	var investmentAmount float64
+	var expectedReturnRate float64
+	var years float64
+
+	fmt.Print("Please Enter your Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Please Enter your years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Please Enter your Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	var futureValue = investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+
+	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
